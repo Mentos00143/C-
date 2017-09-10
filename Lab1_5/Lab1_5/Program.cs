d88fd0f7ed22a03c50e32988dec8ff34d1713760
Mentos <yuramitulya@mail.ru>
2017-09-10 16:57:50 +0300

    Lab1 task 5 C#

---
+++ Lab1_5/Lab1_5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int F1 = 1, F2 = 1, q, n;
+            string N;
+            do
+            {
+                q = 0;
+                Console.Write("Enter N (N>1): ");
+                N = Console.ReadLine();
+                n = int.Parse(N);
+                if(n<1)
+                {
+                    Console.Write("Invalid data!\a");
+                    Console.ReadKey();
+                    Console.Clear();
+                    q = 1;
+                }
+            } while (q != 0);
+            while(n!=F2&&n>F2)
+            {
+                int buf = F2;
+                F2 += F1;
+                F1 = buf;
+            }
+            if (n == F2)
+            {
+                Console.Write("True");
+            }
+            if(n<F2)
+            {
+                Console.Write("False");
+            }
+            Console.ReadKey();
+        }
+    }
+}
